cea87f8da5d47f8cdff79e2570b4d58ea4891138
IWWebSolution <iwwebsolutions@gmail.com>
2025-03-12 12:21:47 +0530

    product image fixed - add product remains; flipbook remains

--- assets/js/main.js
+++ assets/js/main.js
@@ -286,47 +286,6 @@ $(document).ready(function () {
         }
     }
 
-    // Product Image Zoom plugin - product pages
-    $('.product-zoom').each(function () {
-        $(this).elevateZoom({
-            gallery: $(this).attr('data-gallery'),
-            galleryActiveClass: 'active',
-            zoomType: "inner",
-            cursor: "crosshair",
-            zoomWindowFadeIn: 400,
-            zoomWindowFadeOut: 400,
-            responsive: true
-        });
-    
-            // On click change thumbs active item
-        $('.product-gallery-item').on('click', function (e) {
-            $('#product-zoom-gallery').find('a').removeClass('active');
-                $(this).addClass('active');
-
-                e.preventDefault();
-            });
-    
-        var ez = $('#product-zoom').data('elevateZoom');
-    
-            // Open popup - product images
-        $('#btn-product-gallery').on('click', function (e) {
-            if ( $.fn.magnificPopup ) {
-                    $.magnificPopup.open({
-                        items: ez.getGalleryList(),
-                        type: 'image',
-                    gallery:{
-                        enabled:true
-                    },
-                        fixedContentPos: false,
-                        removalDelay: 600,
-                        closeBtnInside: false
-                    }, 0);
-    
-                    e.preventDefault();
-                }
-            });
-    })
-
     // Product Gallery - product-gallery.html 
     if ( $.fn.owlCarousel && $.fn.elevateZoom ) {
         var owlProductGallery = $('.product-gallery-carousel');
